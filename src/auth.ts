e2e973e24f4c97d25c69dfe46dd54ababb105dc7
jujudev7 <penna.julien@gmail.com>
2024-11-04 10:14:39 +0100

    Client-side authentication (SessionProvider, useSession)

--- src/auth.ts
+++ src/auth.ts
@@ -1,11 +1,12 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
+import type { Adapter } from "next-auth/adapters";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import prisma from "./lib/prisma";
-import type { Adapter } from "next-auth/adapters";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  trustHost: true,
   theme: {
     logo: "/logo.png",
   },
