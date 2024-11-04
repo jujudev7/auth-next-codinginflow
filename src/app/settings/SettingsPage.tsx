1baa8ff7bffe965ab91178e1dc6581c4d51f0d87
jujudev7 <penna.julien@gmail.com>
2024-11-04 09:33:21 +0100

    User added in SettingsPage & Adapter added in auth

--- src/app/settings/SettingsPage.tsx
+++ src/app/settings/SettingsPage.tsx
@@ -14,6 +14,7 @@ import { Input } from "@/components/ui/input";
 import { useToast } from "@/components/ui/use-toast";
 import { UpdateProfileValues, updateProfileSchema } from "@/lib/validation";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { User } from "next-auth";
 import { useForm } from "react-hook-form";
 import { updateProfile } from "./actions";
 
--- src/auth.ts
+++ src/auth.ts
@@ -3,11 +3,12 @@ import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import prisma from "./lib/prisma";
+import type { Adapter } from "next-auth/adapters";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   theme: {
     logo: "/logo.png",
   },
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma) as Adapter,
   providers: [Google, GitHub],
 });
