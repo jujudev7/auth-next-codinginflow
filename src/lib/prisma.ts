9a8e07042cb884272f4addbe6ad4cdb731e73771
jujudev7 <penna.julien@gmail.com>
2024-11-04 20:47:08 +0100

    postinstall added & prisma.ts modified

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,17 +3,24 @@ import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
+  if (!process.env.POSTGRES_PRISMA_URL) {
+    throw new Error(
+      "POSTGRES_PRISMA_URL n'est pas défini dans les variables d'environnement.",
+    );
+  }
+
   const neon = new Pool({ connectionString: process.env.POSTGRES_PRISMA_URL });
   const adapter = new PrismaNeon(neon);
   return new PrismaClient({ adapter });
 };
 
 declare global {
-  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+  // Déclare une variable globale pour éviter de recréer PrismaClient
+  // Utilise le type de retour de `prismaClientSingleton` pour s'assurer du type correct
+  var prismaGlobal: ReturnType<typeof prismaClientSingleton> | undefined;
 }
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+// Assigne à `globalThis.prismaGlobal` si en développement, pour réutiliser le client
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
 
 export default prisma;
-
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
